cb69fa51e0c23b9b77d6d13ca011bba1f4bf02cb
Ryne Emory <ryne@bluetent.com>
2016-04-05 19:57:17 -0600

    init

---
+++ scripts/sayHelloWeb.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <link href="../css/phpMM.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <div id="header"><h1>PHP and MySQL: The Missing Manual</h1></div>
+    <div id="example">Example 2-1</div>
+    <div id="content">
+      <h1>Hello, <?php echo $_REQUEST['name']; ?></h1>
+      <p>Great to meet you. Welcome to the beginning f your PHP programming Odyssey.</p>
+    </div>
+    <div id="footer"></div>
+
+  </body>
+</html>
